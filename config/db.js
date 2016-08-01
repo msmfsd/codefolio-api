515926c81fde45cb263668de0badb13c7d3b9b05
Magnus Morris <magnusmorris@icloud.com>
2016-08-01 16:20:23 +1000

    theme option changed to default - TBC v2

--- config/db.js
+++ config/db.js
@@ -43,7 +43,7 @@ module.exports.initDatabase = function(req, res, next) {
         description: 'Developer description ipsum dolor sit amet',
         bio: '<p>Developer bio ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<\/p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.<\/p>',
         layout: {
-          theme: 'dark',
+          theme: 'default',
           displayBgImage: 'yes',
           bgImageURL: 'background-image.jpg'
         },
