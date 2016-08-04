32004cec5869f6a8a33b72bd7482218b55bb995a
Magnus Morris <magnusmorris@icloud.com>
2016-08-04 12:03:06 +1000

    whitelist amend

--- config/cors.js
+++ config/cors.js
@@ -3,7 +3,7 @@
  * Copyright(c) 2016 MSMFSD
  * MIT Licensed
  */
-var whitelist = ['http://ec2-52-65-106-196.ap-southeast-2.compute.amazonaws.com', 'http://codefolio-static.dev', 'http://localhost:3000', 'http://127.0.0.1:3000'];
+var whitelist = ['http://52.65.106.196', 'http://ec2-52-65-106-196.ap-southeast-2.compute.amazonaws.com', 'http://codefolio-static.dev', 'http://localhost:3000', 'http://127.0.0.1:3000'];
 
 module.exports = {
 
