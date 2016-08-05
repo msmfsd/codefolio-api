d459dee55cf93854cff93930fc4e3f172b9d64e6
Magnus Morris <magnusmorris@icloud.com>
2016-08-05 10:06:52 +1000

    travis node v4

--- config/db.js
+++ config/db.js
@@ -41,7 +41,7 @@ module.exports.initDatabase = function(req, res, next) {
         activeProfile: true,
         name: 'Developer Name',
         description: 'Developer description ipsum dolor sit amet',
-        bio: '<p>Developer bio ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<\/p><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.<\/p>',
+        bio: '### Lorem Ipsum\nLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         layout: {
           theme: 'default',
           displayBgImage: 'yes',
